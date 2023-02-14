a63c5e383c7849f723382eb712f9cc16ad73b70b
D1ffic00lt <dm.filinov@gmail.com>
2023-02-14 12:41:21 +0700

    update c++ and .pyx

--- game/all_possible_moves.pyx
+++ game/all_possible_moves.pyx
@@ -1,14 +1,17 @@
-cdef extern from "low-level/cpp_function.cpp" nogil:
-    void get_possible_moves(int xchip, int ychip, string direction, int distance, int& result) ;
+from libcpp.vector cimport vector
+from libcpp.string cimport string
+from libcpp.utility cimport pair
 
-cdef get_cpp_possible_moves(int xchip, int ychip, string direction, int distance, int& result) nogil:
-    return get_possible_moves(xchip, ychip, direction, result)
+cdef extern from "low-level/cpp_function.cpp" nogil:
+    void get_possible_moves(int xchip, int ychip, string direction, int distance, vector[pair[int, int]]& result) ;
+
+cdef get_cpp_possible_moves(int xchip, int ychip, string direction, int distance, vector[pair[int, int]]& result):
+    return get_possible_moves(xchip, ychip, direction, distance, result)
 
 def cpp_get_possible_moves(int xchip, int ychip, string direction, int distance, results=None):
-    cdef int result;
+    cdef vector[pair[int, int]] result;
 
-    with nogil:
-        result = get_cpp_possible_moves(xchip, ychip, direction, distance, result)
+    result = get_cpp_possible_moves(xchip, ychip, direction, distance, result)
 
     if results is not None:
         results.extend(result)
